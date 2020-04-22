a940c91384ec49ebb3ad558c7bc255be5357ec4e
Simon Schmitt <simonschmitt@MacBook-Pro.fritz.box>
2020-04-22 11:41:13 +0200

    split in hello world

--- test/Main.java
+++ test/Main.java
@@ -1,5 +1,9 @@
 public class Main{
   public static void main(String[] args){
+    new Main().start();
+  }
+
+  public void start(){
     System.out.println("Hello World";)
   }
 }
---
+++ test/helloworld.java
@@ -0,0 +1,5 @@
+public class helloworld{
+  public void print(){
+    System.out.println("hello world");
+  }
+}
