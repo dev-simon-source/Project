3a5219215488f95b34a818dd38d412f8ebfc729b
dev-simon-source <64130645+dev-simon-source@users.noreply.github.com>
2020-05-19 23:19:38 +0200

    delete

--- test/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main{
-  public static void main(String[] args){
-    new Main().start();
-  }
-
-  public void start(){
-    System.out.println("Hello World";)
-  }
-}
--- test/helloworld.java
+++
@@ -1,5 +0,0 @@
-public class helloworld{
-  public void print(){
-    System.out.println("hello world");
-  }
-}
